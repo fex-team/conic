31827a96156db1eb3a058ed8d788c8ba5d1b426f
dongtiancheng <dongtiangche@gmail.com>
2015-10-16 16:35:35 +0800

    添加引号

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-var koa = require('koa')
-var app = koa()
+"use strict";
+
+var koa = require('koa');
+var app = koa();
 
 // 监听错误
 app.on("error", function (err) {
-    console.log('服务错误', err)
-})
+    console.log('服务错误', err);
+});
 
 // 抓住未捕获的错误
 process.on('uncaughtException', function (err) {
-    console.error('未捕获错误', err)
+    console.error('未捕获错误', err);
 
     //打印出错误
-    console.log(err)
+    console.log(err);
 
     //打印出错误的调用栈方便调试
-    console.log(err.stack)
-})
+    console.log(err.stack);
+});
 
 // 入口文件
-var templateHtml = require('./src/html.js')
+var templateHtml = require('./src/html.js');
 app.use(function *() {
-    this.type = 'text/html; charset=utf-8'
-    this.body = templateHtml
-})
+    this.type = 'text/html; charset=utf-8';
+    this.body = templateHtml;
+});
 
-module.exports = app.listen(8080)
+module.exports = app.listen(8080);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -43,4 +43,4 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin()
     ]
-}
+};
