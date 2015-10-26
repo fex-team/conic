9395ee8e6f9ddc199b8c6f322c59c2a0313882cf
ascoders <576625322@qq.com>
2015-10-26 13:56:59 +0800

    拆分右侧

---
+++ src/actions/auxiliary-action.js
@@ -0,0 +1,10 @@
+var dispatcher = require('../dispatcher')
+
+module.exports = {
+    showLayoutBox: function (isShow) {
+        dispatcher.dispatch({
+            type: 'showLayoutBox',
+            isShow: isShow
+        })
+    }
+}
--- src/toolbar/right/index.js
+++ src/toolbar/right/index.js
@@ -1,120 +1,32 @@
-var React = require('react');
-var EnterAnimation = require('antd/lib/enter-animation');
-var Tree = require('antd/lib/tree');
-var TreeNode = Tree.TreeNode;
-var Analyse = require('../../util/analyse');
-var _ = require('lodash');
+const React = require('react')
+const Collapse = require('antd/lib/collapse')
+const Panel = Collapse.Panel
 
-function hasChildren (element) {
-    return element.children.length > 0;
-}
+const Tree = require('./tree')
+const Auxiliary = require('./auxiliary')
 
-require('./index.scss');
+const Right = React.createClass({
+    getInitialState: function () {
+        return {}
+    },
 
-var root = require('../../phone-edit/index');
-
-var componentBlackList = ['Edit'];
-
-var treeState = removeBlackList(Analyse(root), componentBlackList);
-
-var treeData = {};
-
-/**
- * 根据黑名单移除多余的组件
- * @param treeSource
- * @param componentBlackList
- * @returns {object}
- */
-function removeBlackList (treeSource, componentBlackList) {
-    var tmpSource = _.cloneDeep(treeSource);
-    var deepest = 0;
-
-
-    _.each(tmpSource, function (component, index) {
-        if (_.isObject(component)) {
-            var name = component.component;
-            if (componentBlackList.join(',').indexOf(name) > -1) {
-
-                var parent = component.parent;
-                var children = component.children;
-                var deep = tmpSource[parent].deep + 1;
-                var parentChildren = tmpSource[parent].children;
-
-                if (deep > deepest) {
-                    tmpSource.__deepest = deep;
-                }
-
-                parentChildren = _.filter(parentChildren, function (value) {
-                    var name = tmpSource[value.key].component;
-
-                    return componentBlackList.join(',').indexOf(name) === -1;
-                });
-
-                children = _.map(children, function (childComponent) {
-                    var key = childComponent.key;
-                    tmpSource[key].parent = parent;
-                    tmpSource[key].deep = deep;
-                    childComponent.parent = parent;
-                    return childComponent;
-                });
-
-                tmpSource[parent].children = parentChildren.concat(children);
-
-                delete tmpSource[index];
-            }
-        }
-    });
-
-    return tmpSource;
-}
-
-
-function buildTreeData (treeState) {
-    var Root = treeState[treeState.__root];
-
-    treeData['Root'] = Root;
-}
-
-
-let ComponentTree = React.createClass({
     render: function () {
-        const animation = {
-            enter: {
-                type: 'right'
-            }
-        };
-
-        let tree;
-
-        function handleCheck(info) {
-            console.log('check: ', info);
-        }
-
-        tree = (
-            <EnterAnimation enter={animation.enter}>
-               <div key="tree" className="layout">
-                   <Tree defaultExpandAll={true} checkable={true} onCheck={handleCheck}>
-                       <TreeNode title="parent 1">
-                           <TreeNode title="leaf" />
-                           <TreeNode title="parent 1-1">
-                               <TreeNode title="parent 2-1">
-                                   <TreeNode title="leaf" />
-                                   <TreeNode title="leaf" />
-                               </TreeNode>
-                               <TreeNode title="leaf" />
-                           </TreeNode>
-                       </TreeNode>
-                   </Tree>
-               </div>
-            </EnterAnimation>
-        );
-
         return (
             <div>
-                {tree}
+                <Collapse defaultActiveKey={["1"]}>
+                    <Panel header={`树状视图`}
+                           key="1">
+                        <Tree/>
+                    </Panel>
+                    <Panel header={`辅助工具`}
+                           key="2">
+                        <Auxiliary/>
+                    </Panel>
+                </Collapse>
             </div>
         )
     }
-});
+})
 
-module.exports = ComponentTree;
+
+module.exports = Right
