ff2959b59b5f4ca5034ea8b83f3845c8f6d58528
ascoders <576625322@qq.com>
2015-10-27 13:10:19 +0800

    update

--- src/toolbar/footer/index.js
+++ src/toolbar/footer/index.js
@@ -5,7 +5,6 @@ module.exports = React.createClass({
     render: function () {
         return (
             <div>
-
                 <div className="layout">Page 贴吧移动端构建平台</div>
             </div>
         )
