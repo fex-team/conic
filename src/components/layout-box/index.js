ad1506e46c121f6cd04df41c289337b896b8823f
ascoders <576625322@qq.com>
2015-10-27 17:55:13 +0800

    拖拽中保持组件选中状态不变

--- src/components/layout-box/index.js
+++ src/components/layout-box/index.js
@@ -58,7 +58,8 @@ let LayoutBox = React.createClass({
                 index: index,
                 customOpts: item.opts || {},
                 dragSource: true,
-                childs: item.childs || []
+                childs: item.childs || [],
+                selected: item.selected || false
             }
             if (item.name === 'LayoutBox') {
                 component = LayoutBox
