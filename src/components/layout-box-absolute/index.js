99bd83a241a0481ffe02b11f828a0cdcd3f3f4bd
ascoders <576625322@qq.com>
2015-10-29 10:50:33 +0800

    优化组件更新效率，无限层级嵌套拖拽完成

--- src/components/layout-box-absolute/index.js
+++ src/components/layout-box-absolute/index.js
@@ -1,15 +1,17 @@
 var React = require('react')
-var $ = require('jquery')
-var Components = require('../../components')
-var Edit = require('../../phone-edit/edit')
-var LayoutBox = require('../../components/layout-box')
-var _ = require('lodash')
+var renderEdit = require('../render-edit')
 
 const defaultStyle = {
     position: 'absolute'
 }
 
 let LayoutBoxAbsolute = React.createClass({
+    mixins: [renderEdit],
+
+    getSelfComponent: function () {
+        return LayoutBoxAbsolute
+    },
+
     getDefaultProps: function () {
         return {
             name: 'LayoutBoxAbsolute',
@@ -46,60 +48,6 @@ let LayoutBoxAbsolute = React.createClass({
                 }
             }
         }
-    },
-
-    getInitialState: function () {
-        return {
-            childs: this.props.childs
-        }
-    },
-
-    componentWillMount: function () {
-        // 为每个子组件生成uniqueKey
-        this.state.childs.map((item, index)=> {
-            item.uniqueKey = index
-        })
-    },
-
-    render: function () {
-        let children = this.state.childs.map((item, index)=> {
-            let component = Components[item.name] || LayoutBoxAbsolute
-            let Editprops = {
-                key: item.uniqueKey,
-                parent: this.props.edit || null,
-                index: index,
-                customOpts: item.opts || {},
-                dragSource: true,
-                childs: item.childs || [],
-                selected: item.selected || false
-            }
-            if (item.name === 'LayoutBox') {
-                component = LayoutBox
-                Editprops.dragTarget = true
-            }
-            if (item.name === 'LayoutBoxAbsolute') {
-                component = LayoutBoxAbsolute
-                Editprops.dragSource = false
-                Editprops.dragSourceAbsolute = true
-                Editprops.dragTarget = true
-            }
-            return React.createElement(Edit, Editprops, React.createElement(component))
-        })
-
-        let nowStyle = Object.assign(this.props.opts.flex.value, this.props.opts.base.value)
-        // 非编辑状态，将拖拽层的绝对定位、left top传给自身组件
-        // 编辑状态，为了视觉效果统一，将绝对定位的absolute left top属性传给父拖拽父级
-        if (!this.props.edit) {
-            nowStyle = Object.assign(nowStyle, defaultStyle)
-            nowStyle.left = this.props.opts.position.left
-            nowStyle.right = this.props.opts.position.right
-        }
-
-        return (
-            <div style={nowStyle}>
-                {children}
-            </div>
-        )
     }
 })
 
--- src/components/layout-box/index.js
+++ src/components/layout-box/index.js
@@ -1,10 +1,13 @@
 var React = require('react')
-var $ = require('jquery')
-var Components = require('../../components')
-var Edit = require('../../phone-edit/edit')
-var _ = require('lodash')
+var renderEdit = require('../render-edit')
 
 let LayoutBox = React.createClass({
+    mixins: [renderEdit],
+
+    getSelfComponent: function () {
+        return LayoutBox
+    },
+
     getDefaultProps: function () {
         return {
             name: 'LayoutBox',
@@ -34,47 +37,6 @@ let LayoutBox = React.createClass({
                 }
             }
         }
-    },
-
-    getInitialState: function () {
-        return {
-            childs: this.props.childs
-        }
-    },
-
-    componentWillMount: function () {
-        // 为每个子组件生成uniqueKey
-        this.state.childs.map((item, index)=> {
-            item.uniqueKey = index
-        })
-    },
-
-    render: function () {
-        let children = this.state.childs.map((item, index)=> {
-            let component = Components[item.name]
-            let Editprops = {
-                key: item.uniqueKey,
-                parent: this.props.edit || null,
-                index: index,
-                customOpts: item.opts || {},
-                dragSource: true,
-                childs: item.childs || [],
-                selected: item.selected || false
-            }
-            if (item.name === 'LayoutBox') {
-                component = LayoutBox
-                Editprops.dragTarget = true
-            }
-            return React.createElement(Edit, Editprops, React.createElement(component))
-        })
-
-        return (
-            <div>
-                <div style={Object.assign(this.props.opts.flex.value,this.props.opts.base.value)}>
-                    {children}
-                </div>
-            </div>
-        )
     }
 })
 
