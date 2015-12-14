e527bb10541668fd36e7b5c5b45fe93a0b319f8e
huangziyi01 <huangziyi01@baidu.com>
2015-12-14 13:07:25 +0800

    quick push

---
+++ src/common.js
@@ -0,0 +1,17 @@
+// antd样式
+require('antd/lib/index.css')
+
+// fontawesome
+require('font-awesome/css/font-awesome.css')
+
+// codemirror
+require('codemirror/lib/codemirror.css')
+require('codemirror/theme/material.css')
+
+// 全局样式
+require('./reset.scss')
+
+// 全局路由
+const routes = require('./router')
+
+module.exports = routes
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-// antd样式
-require('antd/lib/index.css')
-
-// fontawesome
-require('font-awesome/css/font-awesome.css')
-
-// codemirror
-require('codemirror/lib/codemirror.css')
-require('codemirror/theme/material.css')
-
-// 全局样式
-require('./reset.scss')
-
-// 全局路由
-var React = require('react')
-var ReactDOM = require('react-dom')
-var ReactRouter = require('react-router')
-var Router = ReactRouter.Router
-var routes = require('./router')
+const React = require('react')
+const ReactDOM = require('react-dom')
+const routes = require('./common.js')
 
 if (process.env.NODE_ENV === 'development') {
     let Perf = require('react-addons-perf')
